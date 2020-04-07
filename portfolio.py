c427494b70b6718f626400fe3069de6c9866862a
Jake Brehm <jake.m.brehm@gmail.com>
2020-04-06 21:03:07 -0400

    Added a link to the live version of my drawing epicycler project.

--- portfolio.py
+++ portfolio.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def homepage():
     return render_template('index.html')
+
+@app.route('/epicycler/')
+def epicycler():
+    return render_template('epicycler.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
