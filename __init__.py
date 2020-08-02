9eb43a1be5f90183c8dba489ff84e0a3865af0d2
Jake Brehm <jake.m.brehm@gmail.com>
2020-08-01 21:37:36 -0400

    Added code to enforce HTTPS.

--- __init__.py
+++ __init__.py
@@ -1,9 +1,15 @@
 import json
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
+
+@app.before_request
+def force_https():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
 
 @app.route('/')
 def homepage():
